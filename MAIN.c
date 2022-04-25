0c2d60def6834403af4322d39b22413e494d6d67
BalaganiLokendra <104118493+BalaganiLokendra@users.noreply.github.com>
2022-04-24 22:49:42 -0700

    Add files via upload

---
+++ MAIN.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("WELCOME TO B.L THEATRE\n");
+    printf("Enter Your Age: ");
+    scanf("%d",&age);
+    if (age>13)
+    printf("Follow COVID precautions and \nEnjoy KGF CHAPTER-2 movie\n");
+    
+   else 
+    printf("This movie may be harmful to you\n Beast movie is reccomended for you");
+    
+     
+
+    return 0;
+}
